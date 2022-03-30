4802305f1904d1a03e1ba7bdb2223739d1f01d0d
Yan Silveira <ysds@aluno.ifnmg.edu.br>
2022-03-30 10:36:36 -0300

    config inicial

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return (
+    <>
+    <StatusBar style="auto" />
+      <Home />
+    </>
+  );
+}
+
