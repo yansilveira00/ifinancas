17e2af5989ed22f1dd0871aa656ba05b9b39aabb
Yan Silveira <ysds@aluno.ifnmg.edu.br>
2022-03-30 19:58:44 -0300

    atualizando tema

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -2,5 +2,14 @@ export default {
     colors: {
         primary: '#240784',
 
+        shape: '#FFFFFF',
+        title: '#122029',
+        text: '#12202',
+        header: '#240784',
+        background: '#FFFFFF',
+
+        income: '#00C689',
+        expense: '#EA4335',
+
     }
 }
