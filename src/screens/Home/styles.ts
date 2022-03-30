591172a9b4e22e1ea1bf4c633b88f5f2c4519fc1
Yan Silveira <ysds@aluno.ifnmg.edu.br>
2022-03-30 19:58:23 -0300

    Iniciando aplicação de tema

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,17 @@
+import { StyleSheet } from 'react-native';
+
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-      flex: 1;
-      background-color: ${({ theme }) => theme.colors.primary};
-      align-items: center;
-      justify-content: center;
-`;
+    flex: 1;
+    background-color: ;
+    `;
+
+    export const styles = StyleSheet.create({
+      container: {
+        flex: 1;
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: ''
+      }
+    })
