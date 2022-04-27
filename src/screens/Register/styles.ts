fed8ebe08670ee18e621fc57d711ed658625cfee
Phelps671 <74880596+Phelps671@users.noreply.github.com>
2022-04-27 18:16:10 -0300

    Update styles.ts

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -1,7 +1,18 @@
 import styled from "styled-components/native";
+import { RFValue } from 'react-native-responsive-fontsize';
+export const Container = styled.View `
+    background-color: ${({ theme }) => theme.colors.background};
+    flex: 1;
+`;
 
-export const Container = styled.View ``;
-
-export const Header = styled.View ``;
+export const Header = styled.View `
+    width: 100%;
+    height: ${RFValue(75)}px;
+    background-color: ${({ theme }) => theme.colors.primary};
+    
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: center;
+`;
 
 export const Title = styled.View ``;
